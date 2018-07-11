5c4ab5768e5102957e700be9ea67df00c53ecec2
Paweł Szymański <pawel.szymanski@upaid.pl>
2018-07-11 12:20:11 +0100

    Fix price parsing
     * check if price field is not empty
     * add more test cases

--- src/main/java/com/github/jezorko/scrapper/domain/products/ProductsBaseInformationExtractor.java
+++ src/main/java/com/github/jezorko/scrapper/domain/products/ProductsBaseInformationExtractor.java
@@ -5,8 +5,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 
 import java.math.BigDecimal;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 import static lombok.AccessLevel.PACKAGE;
@@ -22,19 +26,23 @@ class ProductsBaseInformationExtractor implements Function<String, List<ProductB
     private final static String PRICE_PER_UNIT_CSS_CLASS = "pricePerUnit";
     private final static String URL_ATTRIBUTE_NAME = "href";
 
+    private final static String NON_NUMERIC_CHARACTERS_REGEX = "[^\\d.]+";
+
     @Override
     public List<ProductBaseInformation> apply(String html) {
-        return Jsoup.parse(html)
-                    .getElementsByClass(PRODUCT_ELEMENT_CLASS_SELECTOR)
-                    .stream()
-                    .map(this::htmlProductToDomainObject)
-                    .collect(toList());
+        return Stream.of(html)
+                     .filter(Objects::nonNull)
+                     .map(Jsoup::parse)
+                     .map(parsed -> parsed.getElementsByClass(PRODUCT_ELEMENT_CLASS_SELECTOR))
+                     .flatMap(Collection::stream)
+                     .map(this::htmlProductToDomainObject)
+                     .collect(toList());
     }
 
     private ProductBaseInformation htmlProductToDomainObject(Element element) {
         Element nameAndDetailsUrlElement = element.selectFirst(PRODUCT_NAME_AND_DETAILS_URL_CSS_SELECTOR);
         BigDecimal pricePerUnit = scapPriceValueFrom(element.getElementsByClass(PRICE_PER_UNIT_CSS_CLASS)
-                                                            .first());
+                                                            .first()).orElse(null);
         return ProductBaseInformation.builder()
                                      .name(nameAndDetailsUrlElement.text())
                                      .detailsUrl(nameAndDetailsUrlElement.attr(URL_ATTRIBUTE_NAME))
@@ -42,8 +50,11 @@ class ProductsBaseInformationExtractor implements Function<String, List<ProductB
                                      .build();
     }
 
-    private BigDecimal scapPriceValueFrom(Element pricePerUnitElement) {
-        return new BigDecimal(pricePerUnitElement.text()
-                                                 .replaceAll("[^\\d.]+", ""));
+    private Optional<BigDecimal> scapPriceValueFrom(Element pricePerUnitElement) {
+        return Optional.ofNullable(pricePerUnitElement)
+                       .map(Element::text)
+                       .map(text -> text.replaceAll(NON_NUMERIC_CHARACTERS_REGEX, ""))
+                       .filter(numberAsString -> !numberAsString.isEmpty())
+                       .map(BigDecimal::new);
     }
 }
--- src/test/groovy/com/github/jezorko/scrapper/domain/products/ProductsBaseInformationExtractorSpecTest.groovy
+++ src/test/groovy/com/github/jezorko/scrapper/domain/products/ProductsBaseInformationExtractorSpecTest.groovy
@@ -1,12 +1,11 @@
 package com.github.jezorko.scrapper.domain.products
 
-
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Unroll
 
-import static TestData.EXAMPLE_PRODUCTS_LIST_DATA
 import static TestData.EXAMPLE_PRODUCTS_LIST_PAGE
+import static com.github.jezorko.scrapper.domain.products.TestData.EXAMPLE_PRODUCTS_LIST_DATA
 
 class ProductsBaseInformationExtractorSpecTest extends Specification {
 
@@ -16,7 +15,7 @@ class ProductsBaseInformationExtractorSpecTest extends Specification {
     @Unroll
     "should return an empty list if HTML #description"() {
         when:
-          def result = extractor.apply ""
+          def result = extractor.apply html
 
         then:
           result.isEmpty()
@@ -24,8 +23,28 @@ class ProductsBaseInformationExtractorSpecTest extends Specification {
         where:
           html     | description
           null     | "is null"
-          ""       | "does not contain any products"
+          ""       | "is empty"
           "<html<" | "is invalid"
+    }
+
+    @Unroll
+    "should return #expectedProduct for HTML with name='#name', url='#url' and price='#price'"() {
+        given:
+          def productHtml = productBaseInformationHtml(name, url, price)
+
+        when:
+          def actualProduct = extractor.apply productHtml
+
+        then:
+          actualProduct.size() == 1 as int
+          actualProduct.get(0) == expectedProduct
+
+        where:
+          name   | url    | price     || expectedProduct
+          'test' | 'test' | '1.0'     || ProductBaseInformation.builder().name('test').detailsUrl('test').pricePerUnitInPounds(1.0G).build()
+          ''     | 'test' | '1.0'     || ProductBaseInformation.builder().name('').detailsUrl('test').pricePerUnitInPounds(1.0G).build()
+          'test' | ''     | '1.0'     || ProductBaseInformation.builder().name('test').detailsUrl('').pricePerUnitInPounds(1.0G).build()
+          'test' | 'test' | 'invalid' || ProductBaseInformation.builder().name('test').detailsUrl('test').pricePerUnitInPounds(null).build()
     }
 
     def "should extract all example products from example HTML"() {
@@ -34,6 +53,38 @@ class ProductsBaseInformationExtractorSpecTest extends Specification {
 
         then:
           EXAMPLE_PRODUCTS_LIST_DATA.forEach { assert result.contains(it) }
+    }
+
+    def "should extract each example product from the HTML representation"() {
+        given:
+          def productAsHtml = productBaseInformationHtml product
+
+        when:
+          def result = extractor.apply productAsHtml
+
+        then:
+          result.size() == 1 as int
+          result.get(0) == product
+
+        where:
+          product << EXAMPLE_PRODUCTS_LIST_DATA
+    }
+
+    static productBaseInformationHtml(ProductBaseInformation product) {
+        productBaseInformationHtml(product.name, product.detailsUrl, product.pricePerUnitInPounds)
+    }
+
+    static productBaseInformationHtml(name, url, price) {
+        """
+            <div class="product">
+                <div class="productNameAndPromotions">
+                    <h3>
+                        <a href="${url}">${name}</a>
+                    </h3>
+                </div>
+                <div class="pricePerUnit">${price}</div>
+            </div>
+        """
     }
 
 }
